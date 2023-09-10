ff18a8c465ea09fe804f86b263d472cc5b15569a
Smurfsx <125309004+Smurfsx@users.noreply.github.com>
2023-09-10 23:01:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+const textToSlide = document.getElementById('textToSlide');
+const textArray = ['Im not a programmer, but I like code 📜', 'Money is everything 💸', 'Rest in peace, see you in the next world "Pleng" 🤍🌷'];
+
+let currentIndex = 0;
+
+function updateText() {
+    textToSlide.textContent = textArray[currentIndex];
+    currentIndex = (currentIndex + 1) % textArray.length;
+}
+
+setInterval(updateText, 800); // Change text every 3 seconds
